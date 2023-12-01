58ac1c386ffe6bc687a62c973a0e27f7bf154efd
bearcito <151670293+bearcito@users.noreply.github.com>
2023-12-01 20:29:27 -0300

    v2.0

--- script.js
+++ script.js
@@ -1,109 +1,169 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const taskList = document.getElementById('taskList');
-    const taskForm = document.getElementById('taskForm');
-    const clearCompletedButton = document.getElementById('clearCompleted');
+    const listaTareas = document.getElementById('listaTareas');
+    const formularioTarea = document.getElementById('formularioTarea');
+    const limpiarCompletadasBoton = document.getElementById('limpiarCompletadas');
 
-    function updateProgressGrid() {
-        const currentDate = new Date();
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        const totalDays = tasks.length;
-        const completedDays = tasks.filter(task => task.completed).length;
-        const progressPercentage = (completedDays / totalDays) * 100;
+    function actualizarBarraProgreso() {
+        const fechaActual = new Date();
+        const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+        const totalDias = tareas.length;
+        const barraProgreso = document.getElementById('barraProgreso');
+        barraProgreso.innerHTML = '';
 
-        const progressGrid = document.getElementById('progressGrid');
-        progressGrid.innerHTML = '';
+        for (let i = 0; i < totalDias; i++) {
+            const celda = document.createElement('div');
+            celda.classList.add('celda-progreso');
 
-        for (let i = 0; i < totalDays; i++) {
-            const cell = document.createElement('div');
-            cell.classList.add('progress-cell');
-
-            if (tasks[i].completed) {
-                cell.style.backgroundColor = '#4CAF50'; // Verde
+            if (tareas[i].completada) {
+                celda.style.backgroundColor = '#4CAF50'; // Verde (si está completada)
             } else {
-                const taskDate = new Date(tasks[i].date);
-                const differenceInMinutes = (currentDate - taskDate) / (1000 * 60);
+                const fechaTarea = new Date(tareas[i].fecha);
+                const diferenciaEnMinutos = (fechaActual - fechaTarea) / (1000 * 60);
 
-                if (differenceInMinutes > 20) {
-                    cell.style.backgroundColor = '#FFC107'; // Amarillo
+                if (diferenciaEnMinutos <= 0) {
+                    celda.style.backgroundColor = '#FFC107'; // Amarillo (si está a tiempo)
                 } else {
-                    cell.style.backgroundColor = '#FF5733'; // Rojo
+                    celda.style.backgroundColor = '#FF5733'; // Rojo (si no está a tiempo)
                 }
             }
 
-            progressGrid.appendChild(cell);
+            barraProgreso.appendChild(celda);
         }
     }
 
-    function renderTasks() {
-        taskList.innerHTML = '';
+    function renderizarTareas() {
+        listaTareas.innerHTML = '';
 
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
 
-        tasks.forEach((task, index) => {
+        tareas.forEach((tarea, index) => {
             const listItem = document.createElement('li');
-            listItem.classList.add('task');
+            listItem.classList.add('tarea');
 
-            const taskDetails = document.createElement('span');
-            taskDetails.textContent = `${task.title} - ${task.date} ${task.alarm ? `(${task.alarm})` : ''}`;
-            taskDetails.addEventListener('click', () => {
-                alert(`Detalles de la tarea:\n${task.title}\nFecha: ${task.date}\nAlarma: ${task.alarm}`);
+            const checkTarea = document.createElement('input');
+            checkTarea.type = 'checkbox';
+            checkTarea.checked = tarea.completada;
+            checkTarea.classList.add('check');
+            checkTarea.addEventListener('change', () => {
+                tarea.completada = !tarea.completada;
+                localStorage.setItem('tareas', JSON.stringify(tareas));
+                renderizarTareas();
+                actualizarBarraProgreso();
             });
 
-            const deleteButton = document.createElement('span');
-            deleteButton.textContent = 'Eliminar';
-            deleteButton.addEventListener('click', () => {
-                tasks.splice(index, 1);
-                localStorage.setItem('tasks', JSON.stringify(tasks));
-                renderTasks();
-                updateProgressGrid();
+            const detallesTarea = document.createElement('span');
+            detallesTarea.textContent = `${tarea.titulo} - ${tarea.fecha} ${tarea.alarma ? `(${tarea.alarma})` : ''}`;
+            detallesTarea.addEventListener('click', () => {
+                alert(`Detalles de la tarea:\n${tarea.titulo}\nFecha: ${tarea.fecha}\nAlarma: ${tarea.alarma}`);
             });
 
-            listItem.appendChild(taskDetails);
-            listItem.appendChild(deleteButton);
-            taskList.appendChild(listItem);
+            const prioridadSelector = document.createElement('select');
+            prioridadSelector.classList.add('prioridad-selector');
+            ['Baja', 'Media', 'Alta'].forEach(opcion => {
+                const option = document.createElement('option');
+                option.value = opcion.toLowerCase();
+                option.text = opcion;
+                prioridadSelector.add(option);
+            });
+            prioridadSelector.value = tarea.prioridad || 'media';
+            prioridadSelector.addEventListener('change', () => {
+                tarea.prioridad = prioridadSelector.value;
+                localStorage.setItem('tareas', JSON.stringify(tareas));
+            });
+
+            const checkLista = document.createElement('input');
+            checkLista.type = 'checkbox';
+            checkLista.checked = tarea.lista;
+            checkLista.classList.add('check-lista');
+            checkLista.addEventListener('change', () => {
+                tarea.lista = checkLista.checked;
+                localStorage.setItem('tareas', JSON.stringify(tareas));
+                renderizarTareas(); // Renderizar de nuevo para reflejar los cambios visuales
+            });
+
+            listItem.appendChild(checkTarea);
+            listItem.appendChild(detallesTarea);
+            listItem.appendChild(prioridadSelector);
+            listItem.appendChild(checkLista);
+            listaTareas.appendChild(listItem);
+
+            // Estilo adicional para las tareas de lista
+            if (tarea.lista) {
+                listItem.classList.add('tarea-lista');
+            }
         });
 
-        updateProgressGrid();
+        actualizarBarraProgreso();
     }
 
-    function clearCompletedTasks() {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        const updatedTasks = tasks.filter(task => !task.completed);
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-        renderTasks();
-        updateProgressGrid();
+    function limpiarTareasCompletadas() {
+        const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+        const tareasActualizadas = tareas.filter(tarea => !tarea.completada);
+        localStorage.setItem('tareas', JSON.stringify(tareasActualizadas));
+        renderizarTareas();
+        actualizarBarraProgreso();
     }
 
-    taskForm.addEventListener('submit', function (event) {
-        event.preventDefault();
+    function establecerAlarma(horaAlarma, tarea) {
+        const ahora = new Date();
+        const fechaAlarma = new Date(ahora.toDateString() + ' ' + horaAlarma);
+        const tiempoHastaAlarma = fechaAlarma - ahora;
 
-        const title = event.target.elements.title.value;
-        const alarm = event.target.elements.alarm.value;
-        const repeat = event.target.elements.repeat.checked;
+        if (tiempoHastaAlarma > 0) {
+            setTimeout(() => {
+                alert(`¡Alarma para la tarea ${tarea.titulo}!`);
+                mostrarNotificacion('Tarea pendiente', `¡Es hora de realizar la tarea "${tarea.titulo}"!`);
+            }, tiempoHastaAlarma);
+        }
+    }
 
-        if (title) {
-            const currentDate = new Date();
-            const newTask = {
-                title,
-                date: currentDate.toISOString().split('T')[0],
-                alarm: alarm || null,
-                repeat,
-                completed: false,
+    function mostrarNotificacion(titulo, mensaje) {
+        if ('Notification' in window) {
+            Notification.requestPermission().then(function (permiso) {
+                if (permiso === 'granted') {
+                    new Notification(titulo, { body: mensaje });
+                }
+            });
+        }
+    }
+
+    formularioTarea.addEventListener('submit', function (evento) {
+        evento.preventDefault();
+
+        const titulo = evento.target.elements.titulo.value;
+        const alarma = evento.target.elements.alarma.value;
+        const repetirDiariamente = evento.target.elements.repetirDiariamente.checked;
+        const prioridad = evento.target.elements.prioridad.value;
+
+        if (titulo) {
+            const fechaActual = new Date();
+            const nuevaTarea = {
+                titulo,
+                fecha: fechaActual.toISOString().split('T')[0],
+                alarma: alarma || null,
+                repetirDiariamente,
+                completada: false,
+                prioridad,
+                lista: false
             };
 
-            const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-            tasks.push(newTask);
-            localStorage.setItem('tasks', JSON.stringify(tasks));
+            const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+            tareas.push(nuevaTarea);
+            localStorage.setItem('tareas', JSON.stringify(tareas));
 
-            renderTasks();
-            updateProgressGrid();
-            event.target.reset();
+            renderizarTareas();
+
+            if (alarma) {
+                establecerAlarma(alarma, nuevaTarea);
+            }
+
+            evento.target.reset();
         } else {
             alert('Por favor, ingresa el título de la tarea.');
         }
     });
 
-    clearCompletedButton.addEventListener('click', clearCompletedTasks);
+    limpiarCompletadasBoton.addEventListener('click', limpiarTareasCompletadas);
 
-    renderTasks();
+    renderizarTareas();
 });
